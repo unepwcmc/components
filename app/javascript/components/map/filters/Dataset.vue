24ba3e34911dcabcbb0b01977f3fb4c4484be414
Will Kocur <will.kocur@unep-wcmc.org>
2018-12-21 14:16:29 +0000

    remove ;

--- app/javascript/components/map/filters/Dataset.vue
+++ app/javascript/components/map/filters/Dataset.vue
@@ -23,7 +23,7 @@
  * This component holds the box for a specific datasets. It controls whether or not it is
  * visible by events that are received by Map.vue that shows/hides individual layers
  */
-import { eventHub } from "../../../vue.js";
+import { eventHub } from "../../../vue.js"
 
 export default {
   name: "layer",
@@ -49,80 +49,80 @@ export default {
     return {
       selected: false,
       layerAdded: false
-    };
+    }
   },
 
   mounted() {
-    eventHub.$on("map-reload-layers", this.reloadLayer); //TODO: move this logic elsewhere?
+    eventHub.$on("map-reload-layers", this.reloadLayer) //TODO: move this logic elsewhere?
     /** TODO change to layers */
-    eventHub.$on("remove-" + this.getDatasetName(), this.removeLayer);
-    eventHub.$on("add-" + this.getDatasetName(), this.addLayer);
+    eventHub.$on("remove-" + this.getDatasetName(), this.removeLayer)
+    eventHub.$on("add-" + this.getDatasetName(), this.addLayer)
   },
 
   destroyed() {
-    eventHub.$off("map-reload-layers", this.reloadLayer);
+    eventHub.$off("map-reload-layers", this.reloadLayer)
   },
 
   computed: {
     isActive() {
-      return this.selected;
+      return this.selected
     },
 
     legendGradient() {
       if (this.layerType === "Raster") {
-        let colours = this.legend.join(", ");
+        const colours = this.legend.join(", ")
 
         return {
           background: `linear-gradient(to right, ${colours})`
-        };
+        }
       } else {
-        let colours = this.cartoColours.join(", ");
+        const colours = this.cartoColours.join(", ")
 
         return {
           background: `linear-gradient(to right, ${colours})`
-        };
+        }
       }
     }
   },
 
   methods: {
     reloadLayer(isStyleLoaded) {
-      this.loadLayer(true, isStyleLoaded);
+      this.loadLayer(true, isStyleLoaded)
     },
 
     loadLayer(forceAdd, isStyleLoaded) {
       if (isStyleLoaded || forceAdd) {
-        this.toggleLayer(false, forceAdd);
+        this.toggleLayer(false, forceAdd)
       }
     },
 
     toggleFilter(boolean) {
       if (typeof boolean == "boolean") {
-        this.selected = boolean;
+        this.selected = boolean
       } else {
-        this.selected = !this.selected;
+        this.selected = !this.selected
       }
 
-      this.toggleLayer(true);
+      this.toggleLayer(true)
     },
 
     toggleLayer(pan, forceAdd) {
       if (!this.layerAdded || forceAdd) {
-        this.createDataset(this.selected);
+        this.createDataset(this.selected)
       }
       if (this.selected) {
-        this.showLayer(pan);
+        this.showLayer(pan)
       } else {
-        this.hideLayer();
+        this.hideLayer()
       }
     },
 
     getDatasetName() {
-      return `dataset_${this.id}_${this.name}`;
+      return `dataset_${this.id}_${this.name}`
     },
 
     createDataset(selected) {
-      this.layerAdded = true;
+      this.layerAdded = true
       /** raster datasets contain just one layer to show */
       if (this.layerType === "Raster") {
         eventHub.$emit("map-create-layer", {
@@ -132,7 +132,7 @@ export default {
           mapbox: {
             tileset: this.mapboxTileset
           }
-        });
+        })
       } else {
         /** Vector datasets contain multiple layers....*/
         for (let ii = 0; ii < this.cartoFilters.length; ii++) {
@@ -141,7 +141,7 @@ export default {
             tables: this.cartoTables,
             colour: this.cartoColours[ii],
             id: this.getDatasetName() + "_" + ii
-          };
+          }
           eventHub.$emit("map-create-layer", {
             name: layer.id,
             type: this.layerType,
@@ -150,41 +150,41 @@ export default {
             mapbox: {
               tileset: this.mapboxTileset
             }
-          });
+          })
         }
       }
     },
 
     addLayer() {
-      this.selected = true;
-      this.toggleLayer();
+      this.selected = true
+      this.toggleLayer()
     },
 
     showLayer(pan) {
       if (this.layerType === "Raster") {
-        eventHub.$emit("map-show-layer", this.getDatasetName());
+        eventHub.$emit("map-show-layer", this.getDatasetName())
       } else {
         for (let ii = 0; ii < this.cartoFilters.length; ii++) {
-          eventHub.$emit("map-show-layer", this.getDatasetName() + "_" + ii);
+          eventHub.$emit("map-show-layer", this.getDatasetName() + "_" + ii)
         }
       }
-      eventHub.$emit("map-set-curr", this.getDatasetName());
+      eventHub.$emit("map-set-curr", this.getDatasetName())
     },
 
     removeLayer() {
-      this.selected = false;
-      this.hideLayer();
+      this.selected = false
+      this.hideLayer()
     },
 
     hideLayer() {
       if (this.layerType === "Raster") {
-        eventHub.$emit("map-hide-layer", this.getDatasetName());
+        eventHub.$emit("map-hide-layer", this.getDatasetName())
       } else {
         for (let ii = 0; ii < this.cartoFilters.length; ii++) {
-          eventHub.$emit("map-hide-layer", this.getDatasetName() + "_" + ii);
+          eventHub.$emit("map-hide-layer", this.getDatasetName() + "_" + ii)
         }
       }
     }
   }
-};
+}
 </script>
