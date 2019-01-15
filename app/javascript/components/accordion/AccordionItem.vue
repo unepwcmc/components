429fae117b6257d49eff854831e1175215a74a9a
Will Kocur <will.kocur@unep-wcmc.org>
2019-01-15 14:10:52 +0000

    make accordion accessible

--- app/javascript/components/accordion/AccordionItem.vue
+++ app/javascript/components/accordion/AccordionItem.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="accordion-item">
-    <a class="accordion-item__toggle flex flex-h-between" @click="toggleAccordionContent()">
-      <span class="accordion-item__title">{{ title }}</span>
+    <button
+      :id="toggleId"
+      :aria-haspopup="true"
+      :aria-controls="contentId"
+      :aria-expanded="isActive"
+      class="accordion-item__toggle flex flex-h-between"
+      @click="toggleAccordionContent()">
+      <label :for="toggleId" class="accordion-item__title">{{ title }}</label>
       <i class="material-icons">{{ arrow }}</i>
-    </a>
+    </button>
     
     <transition name="accordion-toggle">
-      <div v-show="isActive" class="accordion-item__content-wrapper">
+      <div v-show="isActive" role="region" :id="contentId" class="accordion-item__content-wrapper">
         <div class="accordion-item__content">
           <slot></slot>
         </div>
@@ -32,7 +38,9 @@
 
     data () {
       return {
-        isActive: this.open
+        isActive: this.open,
+        toggleId: 'accordion-toggle-' + this.id,
+        contentId: 'accordion-content-' + this.id
       }
     },
 
